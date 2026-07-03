e68e16d4257518e6919e343b77a9c80dbf5a2376
Eka Nurviantorou <eka.nrva22u@gmail.com>
2026-07-04 04:59:47 +0700

    Fix start.sh: recache config with Railway DB env vars

--- docker/start.sh
+++ docker/start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 php artisan storage:link --force
+php artisan config:clear
+php artisan config:cache
 php-fpm -D
 php artisan migrate --force &
 sed "s/listen 80/listen ${PORT:-80}/" -i /etc/nginx/sites-enabled/default
