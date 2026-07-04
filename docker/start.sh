a64ade2a9c1b207e9e1380fd43e2002bdb58363f
Eka Nurviantorou <eka.nrva22u@gmail.com>
2026-07-04 21:13:32 +0700

    Add db:seed --force to start.sh for initial data

--- docker/start.sh
+++ docker/start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 php artisan storage:link --force
 php artisan config:clear
-php artisan migrate --force &
+php artisan migrate --force
+php artisan db:seed --force
 php artisan serve --host=0.0.0.0 --port=${PORT:-8080} --no-reload
